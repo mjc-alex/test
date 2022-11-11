ae9323859fdabe3b5baa1136fc7a32d03fd55b22
魔法少女刘强贵 <jcheng.m@outlook.com>
2022-11-12 01:59:36 +0800

    asm-3-debug

--- asm.c
+++ asm.c
@@ -7,62 +7,48 @@ int main(){
 	// 		:"a"(x),"b"(y)
 	// 	 );
 	// printf("%ld\t%ld\n",x,y);
-	int s = 0, i =0;
-	uint64_t x = 0x31;
-    // asm(
-	// 	// "loop:"
-	// 	// "cmpl $0x3, %2;"
-	// 	// "jge ret;"
-	// 	// "incl %1;"
-	// 	// "addl $0x1, %2;"
-	// 	// "jmp loop;"
-	// 	// "ret:"
-	// 	// "retq;"
-
-	//  	// "loop:"
-	// 	// "incl %1;"
-	// 	// "addl $0x1 , %2;"		
-	// 	// "cmpl $0x3,%2;"
-	// 	// "jl loop;"
-	// 	"cmpl $0x0,%2;"
-	// 	"je inflag;"
-	// 	"jmp exit;"
-	// 	"inflag:incl %1;"
-	// 	"exit:"
-	// 	:"=b" (s)
-	// 	:"b" (s), "a"(i)
-	// 	);
-	// asm(".loop:"
-	//   "cmpl $0, %2;"
-	//   "je .cmp;"
-    //   "shrq  $1,%3;"
-    //   "incl %2;"
-	//   ".cmp:"
-    //   "cmpq $1,%3;"
-    //   "jne .exit"
-    //   "incl %1;"
-    //   ".exit:"
-    //   "cmpl $0x40,%2;"
-    //   "jl .loop;"
-    //   :"=a" (s)
-    //   :"a" (s), "b" (i), "c"(x)
-    //   );
+	int s = 0;
+	size_t n = 6,i = 0;
+	char dest[50],src[50] = "abcdefjhg";
+	int element = 0;
+	// for (i = 0; i<20; i++){
+    // 	src[i] = element;
+	// 	element++;
+  	// }
+	i = 0;
 	asm(".loop:"
-		"cmpl $0, %2;"
-		"je .cmp;"
-		"shrq $1, %3;"
-		".cmp:"
-		"incl %2;"
-		"testq $1, %3;"
-		"jz .exit;"
-		"incl %0;"
-		".exit:"
-		"cmpl $0x40, %2;"
-		"jl .loop;"
-		:"=a" (s)
-		:"a" (s),"b" (i),"c" (x)
-		);
-	P("%d\n",s);
+      "cmpq $0 , %0;"
+      "je .exit;"
+      "leaq (%2,%1,1), %2;"
+      "leaq (%3,%1,1), %3;"
+	  "movb (%3), %%cl;"
+      "movb %%cl, (%2);"
+      "incq %1;"
+      ".exit:"
+      "cmpq %0,%1;"
+      "jl .loop;"
+      :
+      :"a"(n), "b"(i), "S"(dest), "D"(src)
+      :"memory","cc","%ecx"
+      );
+	//memcpy(dest,src,strlen(src));
+	for (i = 0;i < 6; i++){
+		P("%c\n",dest[i]);
+	}
 	return 0;
 }
-		
+// asm(".loop:"
+// 		"cmpl $0, %2;"
+// 		"je .cmp;"
+// 		"shrq $1, %3;"
+// 		".cmp:"
+// 		"incl %2;"
+// 		"testq $1, %3;"
+// 		"jz .exit;"
+// 		"incl %0;"
+// 		".exit:"
+// 		"cmpl $0x40, %2;"
+// 		"jl .loop;"
+// 		:"=a" (s)
+// 		:"a" (s),"b" (i),"c" (x)
+// 		);	
